a33d32b20f7ab0f5c809fb4eed88fadeadc684bb
Yufang J. <debra321@hotmail.com>
2025-07-12 20:02:12 +0800

    feat: add lib production build (#7)
    Co-authored-by: Debra <debra_juan@moneynet.tw>

--- libs/analytics/src/core/Meta.ts
+++ libs/analytics/src/core/Meta.ts
@@ -1,5 +1,5 @@
 import { type BaseAnalyticsEvent } from '../types/events.ts'
-import { Analytics } from './Analytics.ts'
+import { Analytics } from './_Analytics.ts'
 
 export interface MetaPixelConfig {
   measurementId: string
